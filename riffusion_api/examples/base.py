f7fea9f95e2060e8e122ca39427ad254c78b48cf
badim41 <ramdomakk@gmail.com>
2025-02-11 07:42:00 +1200

    Added RiffusionModerationError (3)

--- riffusion_api/examples/base.py
+++ riffusion_api/examples/base.py
@@ -1,7 +1,7 @@
 from riffusion_api import RiffusionAPI
 
-account = RiffusionAPI(sb_api_auth_tokens_0=[]) # provide list or str account token
-track = account.generate(prompt="[Instrumental]", music_style="gitar", input_file=r"E:\Games\for suno\i want minus.mp3")
+account = RiffusionAPI(sb_api_auth_tokens_0="base64-eyJ...") # provide list or str account token
+track = account.generate(prompt="[Instrumental]", music_style="gitar")
 
 print(track.lyrics)
 print(track.result_file_path)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='riffusion_api',
-    version='0.22',
+    version='0.225',
     packages=find_packages(),
     install_requires=[
         'requests~=2.31.0',
